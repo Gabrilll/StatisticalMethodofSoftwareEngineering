3ae4c152b78fbbc9635cac3eb3ffe67d315151ff
Gabri <1034688982@qq.com>
2020-07-26 14:47:27 +0800

    feat: pca_scatter

---
+++ pca_scatter.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data=pd.read_csv('pca_of_matrix.csv')
+
+plt.subplot(2,2,1)
+plt.scatter(data.M1,data.difficulty_index,s=2,c='green',alpha=0.5)
+plt.title("pca_M1")
+
+plt.subplot(2,2,2)
+plt.scatter(data.M2,data.difficulty_index,s=2,c='green',alpha=0.5)
+plt.title("pca_M2")
+
+plt.subplot(2,2,3)
+plt.scatter(data.M3,data.difficulty_index,s=2,c='green',alpha=0.5)
+plt.title("pca_M3")
+
+plt.subplot(2,2,4)
+plt.scatter(data.M4,data.difficulty_index,s=2,c='green',alpha=0.5)
+plt.title("pca_M4")
+
+plt.show()
